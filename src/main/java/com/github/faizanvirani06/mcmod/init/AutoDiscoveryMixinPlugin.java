60c2d478d128c301ba33da8c39a8d0172381cc4e
GitHub Action <action@github.com>
2025-12-27 23:14:50 +0000

    Initialize template repository proper
    Set package name to com.github.faizanvirani06.mcmod
    Set project name to mcmod
    Set modid to mcmod

--- src/main/java/com/github/faizanvirani06/mcmod/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/faizanvirani06/mcmod/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.faizanvirani06.mcmod.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/faizanvirani06/mcmod/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/faizanvirani06/mcmod/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.faizanvirani06.mcmod.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
